e2ba7c4728fef8fdefa625c0473baf79cc3b8b72
guard-mann <>
2025-01-25 23:37:56 +0900

    attatch browser mode

--- torchLinearViz/__init__.py
+++ torchLinearViz/__init__.py
@@ -1,1 +1,2 @@
 from .extract_graph import extract_graph
+from .visualization.serverSocket import start_server as start
--- torchLinearViz/visualization/serverSocket.py
+++ torchLinearViz/visualization/serverSocket.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, send_from_directory
 from flask_socketio import SocketIO
+import socket
 import json
 import time
 import os
+import webbrowser
+from threading import Thread
 
 
 app = Flask(__name__)
@@ -18,6 +21,22 @@ def serve_html():
 def handle_connect():
     print('Client connected')
 
+
+def find_free_port(default_port=5000):
+    """
+    空いているポートを見つける。
+    :param default_port: デフォルトで試みるポート番号
+    :return: 使用可能なポート番号
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        if s.connect_ex(('localhost', default_port)) != 0:
+            return default_port  # デフォルトポートが空いている
+        # デフォルトポートが使われている場合、別のポートを探す
+        for port in range(default_port + 1, default_port + 100):
+            if s.connect_ex(('localhost', port)) != 0:
+                return port
+    raise RuntimeError("No free ports available")
 
 # JSONデータを定期的に送信する処理
 def send_json_data():
@@ -41,8 +60,10 @@ def send_json_data():
 def handle_start():
     socketio.start_background_task(send_json_data)
 
-def main():
-    socketio.run(app, host='0.0.0.0', port=5001)
-
-if __name__ == '__main__':
-    main()
+def start_server(host='0.0.0.0', port=5000, browser=False):
+    free_port = find_free_port(port)
+    if browser:
+        url = f"http://localhost:{free_port}"
+        Thread(target=webbrowser.open, args=(url,), daemon=True).start()
+        
+    socketio.run(app, host=host, port=free_port)
