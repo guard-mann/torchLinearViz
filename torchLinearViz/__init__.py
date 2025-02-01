b7fda5482ef5dfe6d0916e57c14c227e7592ea80
guard-mann <>
2025-02-02 03:15:22 +0900

    init file fixed for class torchLinearViz

--- torchLinearViz/__init__.py
+++ torchLinearViz/__init__.py
@@ -1,3 +1,7 @@
 from .extract_graph import extract_graph
 from .analyse_graph import analyse_graph
 from .visualization.serverSocket import start_server as start
+
+from .torchLinearViz import TorchLinearViz
+
+__all__ = ["TorchLinearViz"]
