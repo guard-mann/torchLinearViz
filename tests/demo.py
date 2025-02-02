01fe91a158fbeb3654a96e35df176aefd4b20826
guard-mann <tomjrekslw3957@gmail.com>
2025-02-02 14:41:36 +0900

    demo code updated

--- tests/demo.py
+++ tests/demo.py
@@ -1,24 +1,41 @@
-import torchLinearViz
+from torchLinearViz import TorchLinearViz
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+import time
 
 class SimpleModel(nn.Module):
     def __init__(self):
         super(SimpleModel, self).__init__()
-        self.fc1 = nn.Linear(10, 20)
-        self.fc2 = nn.Linear(20, 10)
+        self.fc1 = nn.Linear(3, 5)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
         return x
 
 # モデル定義
 model = SimpleModel()
-input_tensor = torch.randn(1, 10)
+input_tensor = torch.randn(1, 3)  # 1 サンプル, 10 次元入力
+target = torch.randn(1, 5)  # 1 サンプル, 10 次元の正解データ (回帰用)
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+loss_fn = nn.MSELoss()  # 回帰タスク用の損失関数
 
-torchLinearViz.extract_graph(model, input_tensor)
 
+# torchLinearViz初期化
+torchlinearviz = TorchLinearViz(model)
+torchlinearviz.start(browser=True)
 
-torchLinearViz.start(browser=True)
+# 学習
+epochs = 10
+for epoch in range(epochs):
+    optimizer.zero_grad()
+    time.sleep(5)
+    output = model(input_tensor)
+    loss = loss_fn(output, target)
+    loss.backward()
+    optimizer.step()
+
+    torchlinearviz.update(model)
+
+torchlinearviz.end()
